c726607c5a2a9981e5baed6d19d96a2c53f13539
User <User@LAB3-3>
2024-03-04 13:53:41 +0200

    .

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using TriviaApp.Services;
+using TriviaApp.View;
 using TriviaApp.ViewModel;
 
 namespace TriviaApp
@@ -18,7 +19,17 @@ namespace TriviaApp
                     fonts.AddFont("WinterDrink.ttf", "WinterDrink");
                 });
             //builder
-           
+            //register services
+            builder.Services.AddSingleton<TriviaAppService>();
+
+            builder.Services.AddTransient<LoginPageViewModel>();
+            builder.Services.AddTransient<UserAdminPageViewModel>();
+            builder.Services.AddTransient<UserQuestionsPageViewModel>();
+            builder.Services.AddTransient<BestScoresPageViewModel>();
+            builder.Services.AddTransient<LoginPage>();
+            builder.Services.AddTransient<UserAdminPage>();
+            builder.Services.AddTransient<UserQuestionsPage>();
+            builder.Services.AddTransient<BestScoresPage>();
 
 #if DEBUG
             builder.Logging.AddDebug();
--- ViewModel/BestScoresPageViewModel.cs
+++ ViewModel/BestScoresPageViewModel.cs
@@ -1,15 +1,48 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Xml.Linq;
 using TriviaApp.Models;
 using TriviaApp.Services;
 
 namespace TriviaApp.ViewModel
 {
-    public class BestScoresPageViewModel:ViewModel
+    public class BestScoresPageViewModel : ViewModel
     {
+        private bool isRefreshing; 
+        public ObservableCollection<User> Users { get; set; }
+        public ICommand LoadUsersCommand { get; private set; }
+        public bool IsRefreshing { get => isRefreshing; set { isRefreshing = value; OnPropertyChanged(); } }
 
+
+        public BestScoresPageViewModel()
+        {
+            Users = new ObservableCollection<User>();
+            LoadUsersCommand = new Command(async () => await
+            LoadUsers());
+        }
+
+        private async Task LoadUsers()
+        {
+            if (IsRefreshing) return;
+            IsRefreshing = true;
+            Users.Clear();
+            foreach (User u in Users)
+            {
+                TriviaAppService service = new TriviaAppService();
+            }
+
+        }
+
+        private void GetUsers()
+        {
+            TriviaAppService service = new TriviaAppService();
+            
+        }
     }
 }
